bdf43b185aaca8073d3b31a49b47b3b4cba81251
William-Santos-Dantas <nidrahdev@protonmail.com>
2022-03-03 19:04:56 -0300

    add getx and percent_indicator

---
+++ lib/modules/home/home_controller.dart
@@ -0,0 +1,7 @@
+import 'package:get/get.dart';
+import '../../core/mixins/loader_mixin.dart';
+import '../../core/mixins/messages_mixin.dart';
+
+class HomeController extends GetxController with LoaderMixin, MessagesMixin {
+  
+}
