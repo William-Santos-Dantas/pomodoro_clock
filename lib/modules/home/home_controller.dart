35dda159ee76db63596f207525b2a44625bc6058
William-Santos-Dantas <nidrahdev@protonmail.com>
2022-03-04 11:43:15 -0300

    Pomodoro Clock Complete

--- lib/modules/home/home_controller.dart
+++ lib/modules/home/home_controller.dart
@@ -1,7 +1,84 @@
+import 'dart:async';
 import 'package:get/get.dart';
-import '../../core/mixins/loader_mixin.dart';
-import '../../core/mixins/messages_mixin.dart';
 
-class HomeController extends GetxController with LoaderMixin, MessagesMixin {
-  
+class HomeController extends GetxController{
+  final _percent = 0.0.obs;
+  get percent => _percent.value;
+  final _currentTime = 0.obs;
+  get currentTime => _currentTime.value;
+
+  final _totalSeconds = 0.obs;
+  get totalSeconds => _totalSeconds.value;
+  final _displayMinutes = 0.obs;
+  get displayMinutes => _displayMinutes.value;
+  final _displaySeconds = ''.obs;
+  get displaySeconds => _displaySeconds.value;
+
+  final _init = false.obs;
+  get init => _init.value;
+
+  final _pause = false.obs;
+
+  // ignore: prefer_typing_uninitialized_variables
+  var interval;
+
+  @override
+  void onReady() {
+    setCurrentTime(25);
+    super.onReady();
+  }
+
+  void setCurrentTime(int time) {
+    _totalSeconds(time * 60);
+    _displayMinutes(totalSeconds ~/ 60);
+    _currentTime(totalSeconds);
+    int seconds = totalSeconds % 60;
+    if (seconds < 10) {
+      _displaySeconds("0$seconds");
+    } else {
+      _displaySeconds(seconds.toString());
+    }
+  }
+
+  void playTimer() {
+    if (currentTime == 0) {}
+    interval = Timer.periodic(
+      const Duration(seconds: 1),
+      (timer) {
+        _init(true);
+        _percent.value += 1 / totalSeconds;
+        if (percent > 1) _percent.value = 1;
+        _currentTime.value -= 1;
+        _displayMinutes(currentTime ~/ 60);
+        int seconds = currentTime % 60;
+        if (seconds < 10) {
+          _displaySeconds("0$seconds");
+        } else {
+          _displaySeconds(seconds.toString());
+        }
+
+        if (currentTime == 0) {
+          _percent(0.0);
+          if (!_pause.value) {
+            setCurrentTime(5);
+            _pause.toggle();
+          } else {
+            setCurrentTime(25);
+            _pause.toggle();
+          }
+        }
+      },
+    );
+  }
+
+  void stop() {
+    if (init) interval.cancel();
+     reset();
+    _init(false);
+  }
+
+  void reset(){
+    setCurrentTime(25);
+    _percent(0);
+  }
 }
--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 
 import 'home_controller.dart';
 
@@ -9,10 +10,139 @@ class HomePage extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Pomodoro Clock'),
+      body: Container(
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xff1542bf),
+              Color(0xff51a8ff),
+            ],
+            begin: FractionalOffset(0.5, 1),
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 30),
+            const Padding(
+              padding: EdgeInsets.only(top: 25),
+              child: Text(
+                'Pomodoro Clock',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40,
+                ),
+              ),
+            ),
+            Obx(
+              () => Expanded(
+                child: CircularPercentIndicator(
+                  percent: controller.percent,
+                  animation: true,
+                  animateFromLastPercent: true,
+                  radius: context.widthTransformer(reducedBy: 70),
+                  lineWidth: 20,
+                  progressColor: Colors.white,
+                  center: Text(
+                    "${controller.displayMinutes}:${controller.displaySeconds}",
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 40,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(30),
+                    topLeft: Radius.circular(30),
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 30, left: 20, right: 20),
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: Column(
+                                children: const [
+                                  Text(
+                                    'Timer',
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                  SizedBox(height: 10),
+                                  Text(
+                                    '25',
+                                    style: TextStyle(fontSize: 80),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Expanded(
+                              child: Column(
+                                children: const [
+                                  Text(
+                                    'Pause Timer',
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                  SizedBox(height: 10),
+                                  Text(
+                                    '5',
+                                    style: TextStyle(fontSize: 80),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 10),
+                        child: SizedBox(
+                          width: context.widthTransformer(reducedBy: 60),
+                          child: Obx(
+                            () => ElevatedButton(
+                              onPressed: controller.init ? controller.stop : controller.playTimer,
+                              style: ElevatedButton.styleFrom(
+                                primary: controller.init ? Colors.red : Colors.blue,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(100),
+                                ),
+                                textStyle: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                ),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(20),
+                                child: Text(controller.init ? 'Stop' : 'Start'),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
-      body: Container(),
     );
   }
 }
